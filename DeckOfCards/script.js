cc295c99f4c1a6ac7c311185be636539c150a007
Brosa-SWE <42998952+Brosa-SWE@users.noreply.github.com>
2021-08-10 17:12:35 +0200

    Add files via upload

---
+++ DeckOfCards/script.js
@@ -0,0 +1,23 @@
+let btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', getCard);
+
+// .catch(err => console.log(err))
+
+function getCard() {
+
+    document.getElementById("card").innerHTML = "";
+
+    let apiuri = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
+        
+    fetch(apiuri)
+        .then(response => response.json())
+        .then(data =>
+        {   
+            let imageurl = data.cards[0].image;
+            let img = document.createElement('img');
+            img.src = imageurl; 
+            document.getElementById('card').appendChild(img);
+        })
+       
+
+}                  
